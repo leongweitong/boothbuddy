b34fe8333854aef7aa920f62b05634d5ef514431
leongweitong <weitong0724@gmail.com>
2025-05-24 14:12:47 +0800

    fix env

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDkkYCcdNzwHHWHtGuk-dFAd-jwU47f7M4",
+  authDomain: "boothbuddy-2359b.firebaseapp.com",
+  projectId: "boothbuddy-2359b",
+  storageBucket: "boothbuddy-2359b.firebasestorage.app",
+  messagingSenderId: "552126727280",
+  appId: "1:552126727280:web:97cc1c296092ddfe1aa0b5",
 };
 
 // Initialize Firebase
